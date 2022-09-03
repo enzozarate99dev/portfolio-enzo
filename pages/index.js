31f532f58ea089a848e72fe72f471667285765bf
Enzo Zarate <enzozarateok66@gmail.com>
2022-09-03 11:16:19 -0300

    commit inicial. instalacion de dependencias

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+
+import Head from 'next/head'
+import Navbar from '../components/Navbar'
+
+const Home = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
+    </div>
+  )
+}
+
+export default Home
