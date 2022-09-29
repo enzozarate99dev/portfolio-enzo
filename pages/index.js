1da45a6470c982df343810d6a12adeef729ea927
Enzo Zarate <enzozarateok66@gmail.com>
2022-09-29 14:05:29 -0300

    se agregaron main skills projects contact about

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
 
+
 import Head from 'next/head'
 import Navbar from '../components/Navbar'
+import Main from '../components/Main'
+import About from '../components/About'
+import Skills from '../components/Skills'
+import Projects from '../components/Projects'
+import Contact from '../components/Contact'
 
 const Home = () => {
   return (
@@ -10,6 +16,11 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
+      <Main />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
     </div>
   )
 }
